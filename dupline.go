6aa24f6154b1c2d4b9c0886e12b83adc3ae47752
lwj1396@163.com <lwj1396@163.com>
2017-03-25 18:27:47 +0800

    一次性读文件

--- dupline.go
+++ dupline.go
@@ -3,12 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
-	/* dup 1
 	counts := make(map[string]int)
+
+	/* dup 1
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
 		if input.Text() == "quit" {
@@ -19,19 +22,32 @@ func main() {
 	*/
 
 	//dup 2
-	counts := make(map[string]int)
-	files := os.Args[1:]
-	if len(files) == 0 {
-		coutline(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
+	/*
+		files := os.Args[1:]
+		if len(files) == 0 {
+			coutline(os.Stdin, counts)
+		} else {
+			for _, arg := range files {
+				f, err := os.Open(arg)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+					continue
+				}
+				coutline(f, counts)
+				f.Close()
 			}
-			coutline(f, counts)
-			f.Close()
+		}
+	*/
+
+	//dup3 一次性读文件
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3 %v \n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
 		}
 	}
 
