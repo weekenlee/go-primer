f4fbc6ab6cc622a5f58f5b47c00cd98733c559bc
lwj1396@163.com <lwj1396@163.com>
2017-03-25 16:00:26 +0800

    重复的行

--- hello.go
+++ hello.go
@@ -4,16 +4,30 @@ import (
 	"fmt"
 	"os"
 	"pra"
+	"strings"
 )
 
-func main() {
+func main1() {
 	fmt.Println("hello world")
 	fmt.Println(pra.Even(2))
 
+	//打印命令行 a
 	var s, sep string
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
 	fmt.Println(s)
+
+	//打印命令行 b
+	sep, s = "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+
+	//打印命令行 c
+	fmt.Println(strings.Join(os.Args[1:], " "))
+
 }
