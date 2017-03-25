e1ae56df39dc03d563976333aefa38ae1715d00b
lwj1396@163.com <lwj1396@163.com>
2017-03-25 15:27:16 +0800

    打印参数

--- hello.go
+++ hello.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"pra"
 )
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println(pra.Even(2))
+
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
