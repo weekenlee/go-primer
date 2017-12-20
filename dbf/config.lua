8fc8f5b1a7e1509e967a9085a4b4c2756fca3407
weeken <lwj1396@163.com>
2017-12-20 19:17:20 +0800

    Create config.lua

---
+++ dbf/config.lua
@@ -0,0 +1,7 @@
+function getconfig()
+	srcroot="Y:\\04-结算运营\\共享资料\\TA\\2_基金交易确认报表\\20171218\\"
+	smfile="TA系统SM对应关系.xls"
+	bankfile="估值系统银行账户对应.xls"
+	todir="."
+	return srcroot, smfile, bankfile, todir
+end
