7319e944f01726565b4ad8da6276dbd5a711ff8a
lwj1396@163.com <lwj1396@163.com>
2017-12-26 20:42:36 +0800

    init方法

--- excel/ex.go
+++ excel/ex.go
@@ -7,16 +7,16 @@ import (
 )
 
 func main() {
-	xlsx, err := excelize.OpenFile("/Users/liweijian/code/py/project/exceltool/output.xlsx")
+	xlsx, err := excelize.OpenFile("/Users/liweijian/code/py/project/exceltool/1_宏创价值成长1期-利润表_私募基金-20171220.xlsx")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	// Get value from cell by given worksheet name and axis.
-	cell := xlsx.GetCellValue("工作表1", "B2")
+	cell := xlsx.GetCellValue("利润表_私募基金", "B2")
 	fmt.Println(cell)
 	// Get all the rows in the Sheet1.
-	rows := xlsx.GetRows("工作表1")
+	rows := xlsx.GetRows("利润表_私募基金")
 	for _, row := range rows {
 		for _, colCell := range row {
 			fmt.Print(colCell, "\t")
---
+++ popcount/popcountTest.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"liweijian.com/popcount"
+)
+
+func main() {
+
+	var p [256]byte = func() (p [256]byte) {
+		for i := range p {
+			p[i] = p[i/2] + byte(i&1)
+		}
+		return p
+	}()
+	fmt.Println(p)
+
+	fmt.Printf("%b\t %d\n", 10>>1, byte(10>>1))
+	fmt.Println(p[byte(10>>(0*8))])
+	fmt.Println(p[byte(10>>(1*8))])
+	fmt.Println(p[byte(10>>(2*8))])
+
+	fmt.Printf("%b\n", 10)
+	fmt.Println(popcount.PopCount(10))
+}
