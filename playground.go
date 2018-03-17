03565c5199a6464f86a8965f74f4aa5d77f6c2f5
lwj1396@163.com <lwj1396@163.com>
2018-03-17 20:15:42 +0800

    类型别名

--- playground.go
+++ playground.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 )
+type id = interface{}
 
-func groupBy(f func(string) string)  func([]string) map[string][]string {
-	return func(list []string) map[string][]string {
-		rmap := make(map[string][]string)
+func groupBy(f func(id) id)  func([]id) map[id][]id {
+	return func(list []id) map[id][]id {
+		rmap := make(map[id][]id)
 		for _, v := range list {
 			rmap[f(v)] = append(rmap[f(v)] , v)
 		}
@@ -14,17 +15,7 @@ func groupBy(f func(string) string)  func([]string) map[string][]string {
 	}
 }
 
-func groupBy2(f func(interface{}) interface{})  func([]interface{}) map[interface{}][]interface{} {
-	return func(list []interface{}) map[interface{}][]interface{} {
-		rmap := make(map[interface{}][]interface{})
-		for _, v := range list {
-			rmap[f(v)] = append(rmap[f(v)] , v)
-		}
-		return rmap
-	}
-}
-
-func f1(v interface{}) interface{} {
+func f1(v id) id {
 	if v=="1" {
 		return "YES"
 	} else {
@@ -32,15 +23,7 @@ func f1(v interface{}) interface{} {
 	}
 }
 
-func f2(v string) string {
-	if v=="1" {
-		return "你好"
-	} else {
-		return "我好"
-	}
-}
-
-func f3(v interface{}) interface{} {
+func f3(v id) id {
 	switch v := v.(type) {
 	case int:
 		if int(v)%2 == 0 {
@@ -54,24 +37,20 @@ func f3(v interface{}) interface{} {
 
 func main() {
 
-	r := groupBy(func(v string) string {
+	r := groupBy(func(v id) id {
 		if v=="1" {
 			return "YES"
 		} else {
 			return "NO"
 		}
-	})([]string{"1","2","3"})
+	})([]id{"1","2","3"})
 	fmt.Println(r)
 
-	r1 := groupBy(f2)([]string{"1","2","3"})
-	fmt.Println(r1)
 
-
-
-	r3 := groupBy2(f1)([]interface{}{"1","2","3"})
+	r3 := groupBy(f1)([]interface{}{"1","2","3"})
 	fmt.Println(r3)
 
 
-	r4 := groupBy2(f3)([]interface{}{1,2,3,4,5,6})
+	r4 := groupBy(f3)([]interface{}{1,2,3,4,5,6})
 	fmt.Println(r4)
 }
