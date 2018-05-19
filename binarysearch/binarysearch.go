6c3dce558dd498cbfcba29a688b2d5a501840286
lwj1396@163.com <lwj1396@163.com>
2018-05-19 12:03:17 +0800

    b

--- binarysearch/binarysearch.go
+++ binarysearch/binarysearch.go
@@ -1,6 +1,22 @@
 package main
 
-func BinarySearch([] int, target int) (int, error) {
-    return -1,nil
+func BinarySearch(array []int, target int) (int, error) {
+    startPos := 0
+    endPos := len(array) - 1
+
+    for true {
+        midPos := (endPos - startPos) / 2
+        midVal := array[midPos]
+
+        if midVal > target {
+            startPos = midPos
+        }else if midVal < target {
+            endPos = midPos
+        }else {
+            return midPos, nil
+        }
+    }
+
+    return -1, nil
 }
 
--- binarysearch/binarysearch_test.go
+++ binarysearch/binarysearch_test.go
@@ -5,7 +5,10 @@
  )
 
  func TestBinarySearch(t *testing.T) {
-
-     array := [1, 3, 4, 5, 6]
-     index , err := BinarySearch(array , 3)
+     array := []int{1, 3, 4, 5, 6}
+     index , err := BinarySearch(array,3)
+     if err != nil {
+         t.Error(err)
+     }
+     t.Log(index)
  }
