82323300c1bf7f7bedc59bed17dbcff6ade40703
lwj1396 <lwj1396@163.com>
2018-07-11 11:35:22 +0800

    wg

---
+++ chan/workergroup.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+type WG struct {
+	main    chan func()
+	allDone chan bool
+}
+
+func New(n int) WG {
+	res := WG {
+		main: make(chan func())
+		allDone: make(chan bool)
+	}
+
+	procDone := make(chan bool)
+	for i := 0; i < n; i++ {
+		go func() {
+			for {
+				f := <-res.main
+				if f == nil {
+					procDone <- true
+					return
+				}
+				f()
+			}
+		}()
+	}
+
+	go func() {
+		for i := 0; i < n; i++ {
+			_ = <- procDone
+		}
+		res.allDone <- true
+	}()
+
+	return res
+}
+
+func (wg WG) Add(f func()) {
+	wg.main <- f
+}
+
+func (wg WG) Wait() {
+	wg.close(main)
+	_ <- allDone
+}
+
+func main() {
+	wg := New(50) //50个worker
+	for i := 0; i < 1000; i++ {
+		wg.Add(func() {
+			fmt.Println("func run")
+		})
+	}
+	wg.Wait()
+}
