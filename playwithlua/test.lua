03caf280364284ccb3d47eb5ce66ce5f3506f040
lwj1396@163.com <lwj1396@163.com>
2019-01-24 21:54:28 +0800

    table

--- playwithlua/test.lua
+++ playwithlua/test.lua
@@ -12,6 +12,21 @@ for l in pairs(s) do
     print(l)
 end
 
+print(#s)
+
+--打印不出来
 for i, l in ipairs(s) do
     print(i, l)
+end
+
+--table.remove(s) 不行
+
+ss={{1,2,3},1,2,3}
+print(#ss)
+for i,l in pairs(ss) do
+    print(i,l)
+end
+--有序可以
+for i, l in ipairs(ss) do
+    print(i,l)
 end
