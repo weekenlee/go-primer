6f01f7678a5a36891c46a613b0849948fd38366f
lwj1396@163.com <lwj1396@163.com>
2019-05-16 17:04:24 +0800

    write

---
+++ 并发写文件/writefile.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"sync"
+)
+
+func produce(data chan int, wg *sync.WaitGroup) {
+	n := rand.Intn(999)
+	data <- n
+	wg.Done()
+}
+
+func consume(data chan int, done chan bool) {
+	f, err := os.Create("concurrent_file")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for d := range data {
+		_, err = fmt.Fprintln(f, d)
+		if err != nil {
+			fmt.Println(err)
+			f.Close()
+			done <- false
+			return
+		}
+	}
+
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		done <- false
+		return
+	}
+
+	done <- true
+}
+
+func main() {
+	data := make(chan int)
+	done := make(chan bool)
+	wg := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go produce(data, &wg)
+	}
+}
