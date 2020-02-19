775e9ed63b530fc69f816536176f0854ac896a9c
lwj1396@163.com <lwj1396@163.com>
2020-02-19 20:57:53 +0800

    go

--- cralwer/test.js
+++ cralwer/test.js
@@ -1,3 +1,5 @@
+start = Date.now()
+
 var numbers = [1,2,3,4,5];
 
 var result = numbers.every(function(value,index,arrays) {
@@ -19,3 +21,25 @@ result = numbers.reduce(function(prev, cur, index, array) {
     return prev + cur;
 });
 console.log(result)
+
+var pattern1 = /at/g
+console.log(pattern1.test("atb"))
+console.log(pattern1.test("atb"))// false, 因为pattern1的索引已经到末尾了，需要新建一个pattern
+console.log(pattern1.test("ab"))
+console.log(pattern1.global)
+console.log(pattern1.ignoreCase)
+console.log(pattern1.multiline)
+console.log(pattern1.lastIndex)
+console.log(pattern1.source)
+
+var pattern2 = /(ab).(cd)/gi
+var matchs = pattern2.exec("abocd")
+if (matchs != null) {
+    console.log(matchs.index)
+    console.log(matchs[0])
+    console.log(matchs[1])
+    console.log(matchs[2])
+}
+
+end = Date.now()
+console.log(end - start) //毫秒数
